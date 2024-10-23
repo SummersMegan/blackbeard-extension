6f18cfd03adf3837cac11e6ac0f5c489cc8acb9f
Megan Summers <50644945+SummersMegan@users.noreply.github.com>
2024-10-23 17:19:12 +0000

    Create copilot extension that recommends GitHub repositories

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/core";
+import { Octokit } from "octokit";
 import express from "express";
 import { Readable } from "node:stream";
 
@@ -9,25 +9,50 @@ app.post("/", express.json(), async (req, res) => {
   const tokenForUser = req.get("X-GitHub-Token");
   const octokit = new Octokit({ auth: tokenForUser });
   const user = await octokit.request("GET /user");
-  console.log("User:", user.data.login);
+
+  // Get public, non-archived github repositories
+  const repos = await octokit.paginate(`GET /orgs/github/repos`, {
+    org: 'github',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    },
+    per_page: 100,
+    type: 'public'
+  })
+  // console.log('Repos name:', JSON.stringify(repos[0].full_name))
+  // console.log('Repos description:', JSON.stringify(repos[0].description))
+
+  // console.log(Object.keys(repos).length)
+  const results = repos.reduce((acc, { full_name, description, archived }) => {
+    if(archived === false) {
+      acc[full_name] = description;
+    }
+    return acc;
+  }, {});
+  // console.log(Object.keys(result).length)
+
+  // console.log('Test: Nil');
+  // console.log("User:", user.data.login);
 
   // Parse the request payload and log it.
   const payload = req.body;
-  console.log("Payload:", payload);
+  // console.log("Payload:", payload);
 
-  // Insert a special pirate-y system message in our message list.
+  // Provide LLM with instructions for responding to user request
   const messages = payload.messages;
   messages.unshift({
     role: "system",
-    content: "You are a helpful assistant that replies to user messages as if you were the Blackbeard Pirate.",
+    content: `You are a assistant that helps the user find the repository that most fits what they're looking for.`,
   });
   messages.unshift({
     role: "system",
-    content: `Start every response with the user's name, which is @${user.data.login}`,
+    content: `These are the repositories you can select from ${JSON.stringify(results)}. You should always return the results as one bullet point per repository and always include links to the repositories.`
+  });
+  messages.unshift({
+    role: "system",
+    content: `Start every response with the user's name, which is @${user.data.login}.`,
   });
 
-  // Use Copilot's LLM to generate a response to the user's messages, with
-  // our extra system messages attached.
   const copilotLLMResponse = await fetch(
     "https://api.githubcopilot.com/chat/completions",
     {
